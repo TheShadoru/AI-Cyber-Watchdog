2b1170203b03773a3f2cccfd8eb9ebd83999c494
Rich Wickersham <143408593+TheShadoru@users.noreply.github.com>
2024-04-12 15:21:20 -0400

    Update shodan_demo.py
    Shodan RAG results now write to a file for storage and further analysis via the web UI

--- shodan_demo.py
+++ shodan_demo.py
@@ -1,6 +1,7 @@
 import ollama
 import shodan_tools
 import cisa_search
+import datetime
 print("""  ____      _                __        __    _       _     ____                      _ 
   / ___|   _| |__   ___ _ __  \ \      / /_ _| |_ ___| |__ |  _ \  ___   __ _  __   _/ |
  | |  | | | | '_ \ / _ \ '__|  \ \ /\ / / _` | __/ __| '_ \| | | |/ _ \ / _` | \ \ / / |
@@ -50,9 +51,11 @@ Creators: Rich Wickersham, Tom Bendien and Joe Carroll
 llm_model = 'llama2'
 
 while True:
+    fdtn = str((datetime.datetime.now().day)) + str((datetime.datetime.now().month)) + str((datetime.datetime.now().year)) + str((datetime.datetime.now().hour)) + str((datetime.datetime.now().minute)) + str((datetime.datetime.now().second))
     companyName = str(input('\nEnter company name: '))
     shodanReport = shodan_tools.shodan_org_scan(companyName)
     print('\nShodan Report:\n{0}\n\n'.format(shodanReport))
+    fullReport = ''
     if len(shodanReport) > 0:
         stream = ollama.chat(
             model=llm_model,
@@ -61,5 +64,14 @@ while True:
         )
         for chunk in stream:
             print(chunk['message']['content'], end='', flush=True)
+            fullReport = fullReport + chunk['message']['content']
+        file = open("shodan_report_{0}.txt".format(fdtn), "a")
+        file.write("\nSearch for: {0}".format(companyName))
+        file.write("\n\n\n")
+        file.write(shodanReport)
+        file.write("\n\n\n")
+        file.write(fullReport)
+        file.close()
     else:
         print("Information not found.")
+    print("Done!\n\n")
