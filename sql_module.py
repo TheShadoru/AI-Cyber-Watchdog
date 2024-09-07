401b6170d0fb6dfd3a8c12878da807adeddcc657
Joe <105601389+yetanotherjoe@users.noreply.github.com>
2024-09-07 08:59:41 -0400

    Bugfixes with SQL module
    Fixed a bug with the SQL module that wrongfully interpreted data as columns and not values.

--- sql_module.py
+++ sql_module.py
@@ -1,11 +1,12 @@
 import sqlite3
 
-def writeData(fdtn, prompt, data, response):
+def writeDB(fdtn, prompt, data, response):
     con = sqlite3.connect('./reports/{0}.sqlite'.format(fdtn))
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS data(prompt, data, response);")
     con.commit()
-    cur.execute("""INSERT INTO data VALUES({0}, {1}, {2})""".format(prompt, data, response))
+    parameters = (prompt, data, response)
+    cur.execute("INSERT INTO data VALUES(?, ?, ?)", parameters)
     con.commit()
     con.close()
 
